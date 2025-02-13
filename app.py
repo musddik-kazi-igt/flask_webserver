b223b78b0771f2a37866514cf2b4dcfc2e8832fc
musddik-kazi-igt <Musddik.Kazi@igtsolutions.com>
2025-02-13 18:15:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return "Hello, Flask build by mk!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
